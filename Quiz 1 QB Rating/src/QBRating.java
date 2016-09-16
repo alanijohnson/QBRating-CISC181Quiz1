671aa9de914697adf316ad468d20cf4156f9a017
Alani <Alani@128.4.213.137>
2016-09-15 23:55:06 -0400

    Added errors for large and negative calculations

--- Quiz 1 QB Rating/src/QBRating.java
+++ Quiz 1 QB Rating/src/QBRating.java
@@ -22,11 +22,37 @@ public class QBRating {
 		
 		//complete calculations
 		double a = 5*((comp/att)-0.3);
+		if (a>2.375){
+			a = 2.375;
+		}
+		else if (a < 0){
+			a = 0;
+		}
 		double b = 0.25 * ((yds/att)-3);
+		if (b>2.375){
+			b = 2.375;
+		}
+		else if (b < 0){
+			b = 0;
+		}
 		double c = 20*td/att;
+		if (c>2.375){
+			c = 2.375;
+		}
+		else if (c < 0){
+			c = 0;
+		}
 		double d = 2.374 - (25*noints/att);
+		if (d>2.375){
+			d = 2.375;
+		}
+		else if (d < 0){
+			d = 0;
+		}
+		
 		
 		double rtg = 100* (a+b+c+d)/6;
+		
 		System.out.print("The Passer Rating is " + rtg );
 		
 	}
